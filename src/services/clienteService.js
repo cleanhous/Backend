1790fe00fb4878b59f2124f91ad4e4e9154b6060
João Pedro <126481770+JoaopedroHolanda@users.noreply.github.com>
2024-10-25 16:29:28 -0300

    Refatorando o código SQL

--- src/services/clienteService.js
+++ src/services/clienteService.js
@@ -1,9 +1,16 @@
+const { hash } = require('bcryptjs');
 const db = require('../db/dbConnection');
 
 class ClienteService{
     async buscaClientePorId(id){
         const [rows] = await db.query(
-            "SELECT * FROM clientes WHERE id = ?",[id]
+            `SELECT * FROM clientes c
+            inner join enderecos_cliente ec
+            on ec.cliente_id = c.id 
+            inner join telefones_cliente tc
+            on tc.cliente_id = c.id
+            WHERE c.id = ?
+            `,[id]
         )
 
         const cliente = rows[0]
@@ -26,10 +33,22 @@ class ClienteService{
         }
 
         const {email,senha, telefone, cep, uf, cidade, logradouro,numero, complemento} = dadosAtualizados
+
+        const senhaHash = await hash(senha,8)
+
+        const atualizaEndereco = await db.query(
+            `UPDADE enderecos_cliente SET cep=?, uf=?, cidade=?, logradouro=?, numero=?,complemento=? WHERE cliente_id = ?`,
+            [cep||cliente.cep, uf||cliente.uf, cidade||cliente.cidade, logradouro||cliente.logradouro,numero|| cliente.numero,complemento||cliente.complemento, id]
+        )
         
+        const atualizaTelefone = await db.query(
+            `UPDATE telefones_cliente SET telefone = ? WHERE cliente_id = ?`,
+            [ telefone || cliente.telefone, id]
+        )
+
         const clienteAtualizado = await db.query(
-            `UPDATE clientes SET email = ?, senha =?, telefone=?, cep=?, uf=?, cidade=?, logradouro=?, numero=?,complemento=?, updatedAt = NOW() WHERE id = ?`,
-            [email || cliente.email,senha||cliente.senha, telefone || cliente.telefone, cep||cliente.cep, uf||cliente.uf, cidade||cliente.cidade, logradouro||cliente.logradouro,numero|| cliente.numero,complemento||cliente.complemento, id]
+            `UPDATE clientes SET email = ?, senha =?, updatedAt = NOW() WHERE id = ?`,
+            [email || cliente.email,senhaHash||cliente.senha, id]
         )
         return clienteAtualizado
     }
--- src/services/prestadorService.js
+++ src/services/prestadorService.js
@@ -6,8 +6,8 @@ class PrestadorService{
         
         const [rows] = await db.query(
             `select p.id,p.nome,p.nota, e.titulo, e.descricao, e.preco from prestadores p
-            inner join especialidades e on e.id = p.id_especialidade
-            inner join servicos s on s.id = e.id_servico where s.titulo = "eletricista"`
+            inner join especialidades e on e.id = p.especialidade_id
+            inner join servicos s on s.id = e.servico_id where s.titulo = "eletricista"`
         )
 
         return rows
@@ -20,17 +20,16 @@ class PrestadorService{
             const dataInicioFormatted = moment(dataInicio).format('YYYY-MM-DD HH:mm:ss');
             const dataFimFormatted = moment(dataFim).format('YYYY-MM-DD HH:mm:ss'); 
     
-            // Consulta para verificar quais prestadores não possuem conflito de datas no período solicitado
             const [rows] = await db.query(`
                 SELECT p.id, p.nome, p.nota, e.titulo, e.descricao, e.preco
                 FROM prestadores p
-                INNER JOIN especialidades e ON e.id = p.id_especialidade
-                INNER JOIN servicos s ON s.id = e.id_servico
+                INNER JOIN especialidades e ON e.id = p.especialidade_id
+                INNER JOIN servicos s ON s.id = e.servico_id
                 WHERE s.titulo = 'eletricista'
                 AND NOT EXISTS (
                     SELECT 1 
                     FROM contratos c
-                    WHERE c.id_prestador = p.id
+                    WHERE c.prestador_id = p.id
                     AND (
                         (c.data_inicio <= ? AND c.data_fim >= ?)
                         OR
