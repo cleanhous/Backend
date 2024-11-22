b1434eea1f772a6ff1272b00da79896042833e3e
afonso7ss <afonso.santos7ass@gmail.com>
2024-11-22 14:09:35 -0300

    bucar contratos

--- src/services/buscaContratosService.js
+++ src/services/buscaContratosService.js
@@ -4,7 +4,18 @@ class BuscaContratosService{
     async buscaTodosContratos(){
         try{
             const [rows] = await db.query(`
-                SELECT * FROM contratos
+                SELECT 
+                    contratos.id AS contrato_id,
+                    contratos.data_inicio,
+                    contratos.data_fim,
+                    prestadores.nome AS prestador_nome,
+                    clientes.nome AS cliente_nome
+                FROM 
+                    contratos
+                INNER JOIN 
+                    prestadores ON contratos.prestador_id = prestadores.id
+                INNER JOIN 
+                    clientes ON contratos.cliente_id = clientes.id;
             `)
             return rows
         }catch(error){
